b3b34ca8f2a51210ed8bac3a1aab96460481a7ad
Danny <20403863+AIPLabs@users.noreply.github.com>
2019-12-13 16:05:05 -0800

    Create New-CMProgram.ps1

---
+++ SCCM/New-CMProgram.ps1
@@ -0,0 +1,23 @@
+# Site configuration
+$SiteCode = "{Site Code}"
+$ProviderMachineName = "{SCCM Server FQDN}"
+
+# Customizations
+$initParams = @{}
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
+$PackageName = "{Package Name}"
+
+New-CMProgram -PackageName $PackageName -StandardProgramName '{Program Name}' -CommandLine 'setup.exe /quiet /forcerestart' -RunType Normal -RunMode RunWithAdministrativeRights -UserInteraction $false -ProgramRunType WhetherOrNotUserIsLoggedOn -DriveMode RenameWithUnc
